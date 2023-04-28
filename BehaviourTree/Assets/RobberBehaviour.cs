f34aad28304819d631f67fdd80a91ab200e9a0df
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-28 20:56:38 +1000

    Add behaviour tree script and node script

---
+++ BehaviourTree/Assets/RobberBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobberBehaviour : MonoBehaviour
+{
+    BehaviourTree tree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tree = new BehaviourTree();
+        Node steal = new Node("Steal Something");
+        Node goToDiamond = new Node("Go to Diamond");
+        Node goToVan = new Node("Go To Van");
+
+        // Construct tree in reverse
+        steal.AddChild(goToDiamond);
+        steal.AddChild(goToVan);
+        tree.AddChild(steal);
+
+        tree.PrintTree();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
